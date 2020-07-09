423441cd89a73b28e7e2868eda2245b7f1ee2665
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-07-10 00:47:49 +0530

    Updates

---
+++ precache-manifest.9fad5d04fe0dbff7c0d75f3fb07429ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2ee5606557f4d3d8500a57ccfeb307",
+    "url": "/crazy-emoji-search/index.html"
+  },
+  {
+    "revision": "6f6d8e3c9ad3e34c47f1",
+    "url": "/crazy-emoji-search/static/css/main.a6d270dc.chunk.css"
+  },
+  {
+    "revision": "cf87f6ef48b053ab79d2",
+    "url": "/crazy-emoji-search/static/js/2.79df7d71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crazy-emoji-search/static/js/2.79df7d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6d8e3c9ad3e34c47f1",
+    "url": "/crazy-emoji-search/static/js/main.4d619084.chunk.js"
+  },
+  {
+    "revision": "dfff08153ff982a4273c",
+    "url": "/crazy-emoji-search/static/js/runtime-main.3542bdbf.js"
+  }
+]);
